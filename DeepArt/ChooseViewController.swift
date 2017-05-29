92a462a697cdb566db0af8e9aa6c153821b9a8b2
guanjhensu <guanjhensu@gmail.com>
2017-05-29 09:54:12 +0800

    StyletransferredTableViewController.swift done

--- DeepArt/ChooseViewController.swift
+++ DeepArt/ChooseViewController.swift
@@ -9,19 +9,29 @@
 import UIKit
 import Alamofire
 
+var filterID: Int = -1
+let filterDic = [
+    ["image": "嘉義街景_OCT1_06.jpg", "artist": "陳澄波", "workName": "嘉義街景", "model": "OCT1_06"],
+    ["image": "Ni_Chiang_Huai_danshui_church.jpg", "artist": "倪蔣懷", "workName": "淡水教堂", "model": "NI_Keelung02"],
+    ["image": "陳植棋_台灣風景.jpg", "artist": "陳植棋", "workName": "台灣風景", "model": "chenchihchi_Taiwan"],
+    ["image": "清流_OCS1_03.jpg", "artist": "陳澄波", "workName": "清流", "model": "OCS1_03"],
+    ["image": "玉山積雪_OW1_05.jpg", "artist": "陳澄波", "workName": "玉山積雪", "model": "OW1_05"],
+    ["image": "陳澄波_OCT1_35.jpg", "artist": "陳澄波", "workName": "淡水夕照", "model": "OCT1_35"],
+//    ["image": "黃榮燦_恐怖的檢查.jpg", "artist": "黃榮燦", "workName": "恐怖的檢查", "model": ""],
+    ["image": "石川欽一郎_台中豐原道.jpg", "artist": "石川欽一郎", "workName": "台中豐原道", "model": "Ishikawa_Taichung"],
+//    ["image": "倪蔣懷_田寮港畔看基隆郵局.jpg", "artist": "倪蔣懷", "workName": "田寮港畔看基隆郵局", "model": ""],
+    ["image": "石川欽一郎_總督府.jpg", "artist": "石川欽一郎", "workName": "總督府", "model": "Ishikawa_taiwan"]]
+
+
 class ChooseViewController: UIViewController {
     
     @IBOutlet weak var selectedImage: UIImageView!
-    @IBOutlet weak var containerViewForPaintings: UIView!
-    
     var newImage: UIImage!
     
     var myScrollView: UIScrollView!
-    var fullSize :CGSize!
+    var fullSize: CGSize!
     
-    let filterDic = [["image": "嘉義街景_OCT1_06.jpg", "artist": "陳澄波", "workName": "嘉義街景"],["image": "Ni_Chiang_Huai_danshui_church.jpg", "artist": "倪蔣懷", "workName": "淡水教堂"],["image": "陳植棋_台灣風景.jpg", "artist": "陳植棋", "workName": "台灣風景"],["image": "清流_OCS1_03.jpg", "artist": "陳澄波", "workName": "清流"],["image": "玉山積雪_OW1_05.jpg", "artist": "陳澄波", "workName": "玉山積雪"],["image": "陳澄波_OCT1_35.jpg", "artist": "陳澄波", "workName": "淡水夕照"],["image": "黃榮燦_恐怖的檢查.jpg", "artist": "黃榮燦", "workName": "恐怖的檢查"],["image": "石川欽一郎_台中豐原道.jpg", "artist": "石川欽一郎", "workName": "台中豐原道"],["image": "倪蔣懷_田寮港畔看基隆郵局.jpg", "artist": "倪蔣懷", "workName": "田寮港畔看基隆郵局"],["image": "石川欽一郎_總督府.jpg", "artist": "石川欽一郎", "workName": "總督府"]]
-    
-   
+
     var filter:[UIImageView] = []
     var newFilter: UIImageView? = nil
     
@@ -35,7 +45,7 @@ class ChooseViewController: UIViewController {
         myScrollView = createFilterChooseBar(scrollview: myScrollView)
         self.view.addSubview(myScrollView)
         
-        for i in 0...9 {
+        for i in 0...7 {
             // make frame with shadow
             let whiteView = UIView(frame: CGRect(origin: CGPoint(x: (24 + i * 84),y: 0), size: CGSize(width: 60, height: 110)))
             whiteView.backgroundColor=UIColor.white
@@ -62,22 +72,10 @@ class ChooseViewController: UIViewController {
             btn.backgroundColor = nil
             btn.frame = CGRect(x: (24 + i * 84), y: 0, width: 60, height: 110)
             btn.tag = i
-            btn.addTarget(self, action: #selector(clickShowArtist), for: .touchUpInside)
+            btn.addTarget(self, action: #selector(showArtistName), for: .touchUpInside)
             self.myScrollView.addSubview(btn)
 
         }
-
-        
-        
-        // add artist name
-        var tapGesture = UITapGestureRecognizer()
-        var FirstTapDone: Bool = false
-        var count: Int = 0
-        var CaseWhatToDo: Int = 0 //0：沒有按過螢幕。1：出現畫家名字。2：決定選擇濾鏡
-        
-
-        
-        
 
     }
     
@@ -100,25 +98,19 @@ class ChooseViewController: UIViewController {
         
         return myScrollView
     }
-    
-    func clickShowArtist(sender:UIButton!) {
-        print("Button Clicked")
-        showArtistName(index: sender.tag)
-    }
-    
-    func showArtistName(index i: Int){
+   
+    func showArtistName(sender:UIButton!){
+        filterID = sender.tag
         
-        let artistName = UILabel(frame: CGRect(origin: CGPoint(x: (24 + i * 84),y :110), size: CGSize(width: 60, height: 110)))
+        let artistName = UILabel(frame: CGRect(origin: CGPoint(x: (24 + filterID * 84),y :110), size: CGSize(width: 60, height: 110)))
         artistName.textAlignment = .center
-        artistName.text = filterDic[i]["artist"]!
+        artistName.text = filterDic[filterID]["artist"]!
         artistName.textColor = .white
         artistName.backgroundColor = UIColor(red: 76, green: 76, blue: 76, a: 0.5)
         artistName.font = artistName.font.withSize(10)
-     
-        
-        
+
         let top = CGAffineTransform(translationX: 0, y: -110)
-        UIView.transition(with: self.myScrollView, duration: 0.5, options: UIViewAnimationOptions.curveEaseOut, animations: {
+        UIView.transition(with: self.myScrollView, duration: 0.2, options: UIViewAnimationOptions.curveEaseOut, animations: {
             self.myScrollView.addSubview(artistName)
             artistName.transform = top
             
@@ -127,23 +119,23 @@ class ChooseViewController: UIViewController {
             // go to "transformedTableViewController.swift"
             let btn = UIButton(type: .custom) as UIButton
             btn.backgroundColor = nil
-            btn.frame = CGRect(x: (24 + i * 84), y: 0, width: 60, height: 110)
-            btn.tag = i
+            btn.frame = CGRect(x: (24 + filterID * 84), y: 0, width: 60, height: 110)
             btn.addTarget(self, action: #selector(self.filterSelected), for: .touchUpInside)
             self.myScrollView.addSubview(btn)
         
         })
     }
 
-    func filterSelected(sender:UIButton!){
-        print("go next page~")
+    func filterSelected(sender: UIButton!){
+        let styleTransferArtistSegueID = filterDic[filterID]["artist"]!
+        self.performSegue(withIdentifier: styleTransferArtistSegueID, sender: nil)
     
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "chooseOnePainting" {
-            let controller = segue.destination as! PaintingsTableViewController
-            controller.photoToPaintingsTableViewController = newImage
+        if segue.identifier == filterDic[filterID]["artist"]! {
+            let controller = segue.destination as! StyleTransferredTableViewController
+            controller.uploadImage = newImage
         }
     }
 }
@@ -220,39 +212,3 @@ extension UIView {
         self.layer.shadowRadius = 1
     }
 }
-
-
-
-//
-//        @IBAction func TapTheScreen(_ sender: UITapGestureRecognizer){
-//            print("tap the screen")
-//            if synth.isSpeaking && CaseWhatToDo != 0{
-//                CaseWhatToDo = 1
-//                synth.stopSpeaking(at: AVSpeechBoundary.word)
-//                //按一下，馬上停止語音輸出
-//            }
-//            switch CaseWhatToDo {
-//            case 0:
-//                synth.stopSpeaking(at: AVSpeechBoundary.word)
-//                let ReadString:String = "請問您要直接開啟相機進行辨識嗎？"
-//                TTScase = 1
-//                myTTS(mystring: ReadString)
-//            case 1:
-//                synth.stopSpeaking(at: AVSpeechBoundary.word)
-//            case 2:
-//                if audioEngine.isRunning {
-//                    //錄音停止
-//                    audioEngine.stop()
-//                    recognitionRequest?.endAudio()
-//                    tapView.isUserInteractionEnabled = false
-//
-//                } else {
-//                    isUserStringChanged = false
-//                    //開始錄音並進行語音辨識
-//                    startRecording()
-//                }
-//            default:
-//                break
-//            }
-//            CaseWhatToDo = 2
-//        }
---
+++ DeepArt/StyleTransferredTableViewController.swift
@@ -0,0 +1,111 @@
+//
+//  StyleTransferredTableViewController.swift
+//  DeepArt
+//
+//  Created by 蘇冠禎 on 2017/5/28.
+//  Copyright © 2017年 蘇冠禎. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class StyleTransferredTableViewController: UITableViewController {
+
+    var uploadImage: UIImage!
+    var transformedImageView = UIImageView(image: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        transformedImageView.frame = CGRect(origin: CGPoint(x: 0,y: 0), size: CGSize(width: 375, height: 375))
+        view.addSubview(self.transformedImageView)
+        
+        let randomImageName: String = filterDic[filterID]["model"]! + "_____" + randomString(length: 5)
+        upload(image: (uploadImage.resized(withPercentage: 0.4)!), name: randomImageName)
+
+    }
+
+    // upload photo to server
+    func upload(image: UIImage, name: String) {
+        guard let imageData = UIImageJPEGRepresentation(image, 1.0) else {
+            print("Could not get JPEG representation of UIImage")
+            return
+        }
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(imageData,
+                                         withName: "upload",
+                                         fileName: name,
+                                         mimeType: "image/jpeg")
+                
+        },
+            to: "http://127.0.0.1:3000",
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.responseJSON  { response in
+                        debugPrint(response)
+                        // style transfer success!
+                        self.load(requestImageName: name)
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+        })
+    }
+    
+    // load photo to client
+    func load(requestImageName: String){
+        let requestUrl = "http://localhost:3000/imagesToApp/" + requestImageName + ".jpg"
+        Alamofire.request( requestUrl, method: .get)
+            .downloadProgress { progress in
+                print("Download Progress: \(progress.fractionCompleted)")
+            }
+            .responseData { response in
+                if let data = response.result.value {
+                    let image = UIImage(data: data)
+                    if image != nil {
+                        self.transformedImageView.image = image
+                        
+                        print("image load success")
+                    } else {
+                        print("fail to find transferred image")
+                    }
+                }
+        }
+    }
+    
+    func randomString(length:Int) -> String {
+        var s: String = ""
+        for _ in (1...length) {
+            s.append(String(arc4random()))
+        }
+        return s
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+// resize image
+extension UIImage {
+    func resized(withPercentage percentage: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: size.width * percentage, height: size.height * percentage)
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+    func resized(toWidth width: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
+
